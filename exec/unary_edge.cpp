1b55a942f622743e73918b750dd14d7a0b644b4f
qiaozhijian <935514540@qq.com>
2021-02-04 22:11:08 +0800

    update

--- exec/unary_edge.cpp
+++ exec/unary_edge.cpp
@@ -2,15 +2,49 @@
 // Created by qzj on 2021/2/4.
 //
 #include <iostream>
+#include "glog/logging.h"
+#include "global_defination/global_defination.h"
 #include "optimizer.h"
-
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "main" << endl;
+    google::InitGoogleLogging(argv[0]);
+    FLAGS_log_dir = WORK_SPACE_PATH + "/Log";
+    FLAGS_alsologtostderr = true;
+    FLAGS_colorlogtostderr = true;
+    FLAGS_log_prefix = true;
+    FLAGS_logbufsecs = 0;
 
-    Optimizer optimizer;
+    Optimizer optimizer(false, 100);
+    
+//    设定初始位姿
+    Eigen::Isometry3d groundTruth = Eigen::Isometry3d::Identity();
 
+    Vector6d noise;
+    double transNoise = 0.1;
+    double rotNoise = 0.01;
+    noise << transNoise,transNoise,transNoise,rotNoise,rotNoise,rotNoise;
+    Optimizer::sample_noise_from_se3(noise);
+
+    Eigen::Isometry3d initPose = Optimizer::sample_noise_from_se3(noise) * groundTruth;
+    Eigen::Isometry3d imuPose = Optimizer::sample_noise_from_se3(noise) * groundTruth;
+    Eigen::Isometry3d cameraPose = Optimizer::sample_noise_from_se3(noise) * groundTruth;
+    Eigen::Isometry3d lidarPose = Optimizer::sample_noise_from_se3(noise) * groundTruth;
+
+    g2o::VertexSE3* v1 = optimizer.addSE3Node(groundTruth, 0, false);
+
+    Matrix6d info = Optimizer::calcInfoMat(noise);
+    optimizer.addSE3Edge(v1, imuPose, info, 0);
+    optimizer.addSE3Edge(v1, cameraPose, info, 0);
+    optimizer.addSE3Edge(v1, lidarPose, info, 0);
+
+    LOG(INFO)<< "Before optimize, \n" << v1->estimate().matrix() <<endl;
+
+    optimizer.runOptimizer(0);
+
+    Eigen::Isometry3d nodePose = v1->estimate();
+
+    LOG(INFO)<< "After optimize, \n" << nodePose.matrix() <<endl;
 }
--- include/optimizer.h
+++ include/optimizer.h
@@ -5,11 +5,57 @@
 #ifndef POSE_GRAPH_OPTIMIZER_H
 #define POSE_GRAPH_OPTIMIZER_H
 
+#include <g2o/core/sparse_optimizer.h>
+#include <g2o/types/slam3d/types_slam3d.h>
+#include <g2o/types/slam3d/edge_se3.h>
+#include <g2o/types/slam3d/vertex_se3.h>
+#include <g2o/core/factory.h>
+#include <g2o/core/block_solver.h>
+#include <g2o/core/linear_solver.h>
+#include <g2o/core/robust_kernel.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/solvers/cholmod/linear_solver_cholmod.h>
+
+typedef Eigen::Matrix<double, 6, 6> Matrix6d;
+typedef Eigen::Matrix<double, 6, 1> Vector6d;
 
 class Optimizer {
 
+public:
 
+    Optimizer(const bool& Verbose, const int& maxIteration);
 
+    g2o::VertexSE3* addSE3Node(const Eigen::Isometry3d &pose, const int KFid, const bool& isFixed);
+
+    void addSE3Edge(g2o::VertexSE3* &v1, g2o::VertexSE3* &v2, const Eigen::Isometry3d &relative_pose,
+                               const Matrix6d &info_matrix, int level);
+
+    void addSE3Edge(g2o::VertexSE3* &v1, const Eigen::Isometry3d &iso_pose,
+                               const Matrix6d &info_matrix, int level);
+
+    ulong getNodeSize();
+
+    ulong getEdgeSize();
+
+    void runOptimizer(int level=0);
+
+    bool removeSE3Node(g2o::VertexSE3 *node);
+
+    void addSE3PlaneEdge(g2o::VertexSE3* &v1, int level);
+
+    static Matrix6d calcInfoMat(Matrix6d cov);
+
+    static Matrix6d calcInfoMat(Vector6d cov);
+
+    static Eigen::Isometry3d sample_noise_from_se3(Vector6d& cov );
+
+private:
+
+    g2o::SparseOptimizer* graph;
+
+    bool _Verbose;
+
+    int _MaxIterationTimes;
 };
 
 
